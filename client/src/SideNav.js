16a9bba5e3f1682b7add7dfd5ea1248c74aa1a6e
greenfe2 <greenfe2.wwu.edu>
2019-11-07 11:39:56 -0800

    CSS change and removed old files

--- client/src/SideNav.js
+++ client/src/SideNav.js
@@ -5,25 +5,49 @@ import './spotistyle.css'
 function SideNav() {
     return(
         <div className="sidenav">
-            <div class="nav-wrapper">
-              <div class="dropdown">
+            <div className="nav-wrapper">
+            <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for graphs">
+            </input>
+              <div className="dropdown">
                 <a href="#">Top 5</a>
+                  <div className="dropdownselection">
+                    <a href = "#">- Artists</a>
+                    <a href = "#">- Songs</a>
+                    <a href = "#">- Albums</a>
+                  </div>
               </div>
-              <div class="dropdown">
+              <div className="dropdown">
                 <a href="#">Totals</a>
+                <div className="dropdownselection">
+                  <a href = "#">- Artists</a>
+                  <a href = "#">- Songs</a>
+                  <a href = "#">- Albums</a>
+                </div>
               </div>
-              <div class="dropdown">
+              <div className="dropdown">
                 <a href="#">Genre Breakdown</a>
               </div>
-              <div class="dropdown">
+              <div className="dropdown">
                 <a href="#">Hipster Rating</a>
+                <div className="dropdownselection">
+                  <a href = "#">- Average popularity</a>
+                  <a href = "#">- Most popular</a>
+                  <a href = "#">- Most underground</a>
+                </div>
               </div>
-              <div class="dropdown">
+              <div className="dropdown">
                 <a href="#">Averages</a>
+                <div className="dropdownselection">
+                  <a href = "#">- Average danceability</a>
+                  <a href = "#">- Average happiness</a>
+                  <a href = "#">- Average energy</a>
+                </div>
               </div>
             </div>
         </div>
     )
 }
+
+
 
 export default SideNav
