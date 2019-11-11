5fb1a24b7d2df5c3229e644514be31bf1abe58ad
greenfe2 <greenfe2@wwu.edu>
2019-11-11 15:31:34 -0800

    change search bar

--- client/src/SideNav.js
+++ client/src/SideNav.js
@@ -9,19 +9,19 @@ function SideNav() {
               <input className = "searchInput" type="text" id="myInput" onKeyUp={searchGraphs} placeholder="Search for graphs">
               </input>
               <ul id="myMenu">
-                <li className = "graphdropdown"><a href="#">Top 5</a></li>
-                <li className = "graphdropdown"><a href="#">- Artists</a></li>
-                <li className = "graphdropdown"><a href="#">- Songs</a></li>
-                <li className = "graphdropdown"><a href = "#">- Albums</a></li>
+                <li className = "graphdropdown"><a className = "sideNavParent" href="#">Top 5</a></li>
+                <li className = "graphdropdown"><a className = "sideNavChild" href="#">- Artists</a></li>
+                <li className = "graphdropdown"><a className = "sideNavChild" href="#">- Songs</a></li>
+                <li className = "graphdropdown"><a className = "sideNavChild" href = "#">- Albums</a></li>
                 <li className = "graphdropdown"><a href="#">Genre Breakdown</a></li>
                 <li className = "graphdropdown"><a href="#">Hipster Rating</a></li>
-                <li className = "graphdropdown"><a href="#">- Average popularity</a></li>
-                <li className = "graphdropdown"><a href="#">- Most popular</a></li>
-                <li className = "graphdropdown"><a href="#">- Most underground</a></li>
+                <li className = "graphdropdown"><a className = "sideNavChild" href="#">- Average popularity</a></li>
+                <li className = "graphdropdown"><a className = "sideNavChild" href="#">- Most popular</a></li>
+                <li className = "graphdropdown"><a className = "sideNavChild" href="#">- Most underground</a></li>
                 <li className = "graphdropdown"><a href="#">Averages</a></li>
-                <li className = "graphdropdown"><a href="#">- Average danceability</a></li>
-                <li className = "graphdropdown"><a href="#">- Average happiness</a></li>
-                <li className = "graphdropdown"><a href="#">- Average energy</a></li>
+                <li className = "graphdropdown"><a className = "sideNavChild" href="#">- Average danceability</a></li>
+                <li className = "graphdropdown"><a className = "sideNavChild" href="#">- Average happiness</a></li>
+                <li className = "graphdropdown"><a className = "sideNavChild" href="#">- Average energy</a></li>
               </ul>
             </div>
         </div>
