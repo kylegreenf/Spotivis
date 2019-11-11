57853f34d8938803dbd7c6e86730348c33d97061
greenfe2 <greenfe2@wwu.edu>
2019-11-11 12:17:33 -0800

    search menu works

--- client/src/SideNav.js
+++ client/src/SideNav.js
@@ -5,77 +5,24 @@ import './spotistyle.css'
 function SideNav() {
     return(
         <div className="sidenav">
-            <div className="nav-wrapper">
-            <input className = "searchInput" type="text" id="myInput" onKeyUp={searchGraphs} placeholder="Search for graphs">
-            </input>
-              <div className = "graphdropdownwrapper">
-                  <div className="graphdropdown">
-                    <div className="graphdropdowncategory graphdropdownhighlight">
-                      <a href="#">Top 5</a>
-                    </div>
-                      <div className="graphdropdownselection graphdropdownhighlight">
-                        <a href = "#">- Artists</a>
-                          </div>
-                      <div className="graphdropdownselection graphdropdownhighlight">
-                        <a href = "#">- Songs</a>
-                          </div>
-                      <div className="graphdropdownselection graphdropdownhighlight">
-                        <a href = "#">- Albums</a>
-                      </div>
-                  </div>
-                  <div className="graphdropdown">
-                    <div className="graphdropdowncategory graphdropdownhighlight">
-                      <a href="#">Totals</a>
-                    </div>
-                    <div className="graphdropdownselection">
-                      <div className="graphdropdownselection graphdropdownhighlight">
-                        <a href = "#">- Artists</a>
-                      </div>
-                      <div className="graphdropdownselection graphdropdownhighlight">
-                        <a href = "#">- Songs</a>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="graphdropdown">
-                    <div className="graphdropdowncategory graphdropdownhighlight">
-                      <a href="#">Genre Breakdown</a>
-                    </div>
-
-                  </div>
-                  <div className="graphdropdown">
-                    <div className="graphdropdowncategory graphdropdownhighlight">
-                      <a href="#">Hipster Rating</a>
-                    </div>
-
-                    <div className="graphdropdownselection">
-                      <div className="graphdropdownselection graphdropdownhighlight">
-                        <a href = "#">- Average popularity</a>
-                      </div>
-                      <div className="graphdropdownselection graphdropdownhighlight">
-                        <a href = "#">- Most popular</a>
-                      </div>
-                      <div className="graphdropdownselection graphdropdownhighlight">
-                        <a href = "#">- Most underground</a>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="graphdropdown">
-                    <div className="graphdropdowncategory graphdropdownhighlight">
-                      <a href="#">Averages</a>
-                    </div>
-                    <div className="graphdropdownselection">
-                      <div className="graphdropdownselection graphdropdownhighlight">
-                        <a href = "#">- Average danceability</a>
-                      </div>
-                      <div className="graphdropdownselection graphdropdownhighlight">
-                        <a href = "#">- Average happiness</a>
-                      </div>
-                      <div className="graphdropdownselection graphdropdownhighlight">
-                        <a href = "#">- Average energy</a>
-                      </div>
-                    </div>
-                  </div>
-                  </div>
+          <div class="row">
+              <input className = "searchInput" type="text" id="myInput" onKeyUp={searchGraphs} placeholder="Search for graphs">
+              </input>
+              <ul id="myMenu">
+                <li className = "graphdropdown"><a href="#">Top 5</a></li>
+                <li className = "graphdropdown"><a href="#">- Artists</a></li>
+                <li className = "graphdropdown"><a href="#">- Songs</a></li>
+                <li className = "graphdropdown"><a href = "#">- Albums</a></li>
+                <li className = "graphdropdown"><a href="#">Genre Breakdown</a></li>
+                <li className = "graphdropdown"><a href="#">Hipster Rating</a></li>
+                <li className = "graphdropdown"><a href="#">- Average popularity</a></li>
+                <li className = "graphdropdown"><a href="#">- Most popular</a></li>
+                <li className = "graphdropdown"><a href="#">- Most underground</a></li>
+                <li className = "graphdropdown"><a href="#">Averages</a></li>
+                <li className = "graphdropdown"><a href="#">- Average danceability</a></li>
+                <li className = "graphdropdown"><a href="#">- Average happiness</a></li>
+                <li className = "graphdropdown"><a href="#">- Average energy</a></li>
+              </ul>
             </div>
         </div>
     )
